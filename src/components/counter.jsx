5f00e30d532e43264e7628a1b5a2ec841073b4af
khanraakash <khanraakash@gmail.com>
2019-07-18 21:48:26 +0530

    Part 1 completed

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,32 +1,36 @@
-import React,{Component} from 'react';
- class Counter extends  Component {
-     state = {
-         count: 0,
-         tags: ['tag1', 'tag2', 'tag3']
-     };
-     handelIncrement = product =>{
-         console.log(product);
-         this.setState({count: this.state.count + 1})
-     };
-   render() {
-       return(
-           <div>
-               <span  className={this.getBadgeCLasses()}>{this.formatCount()}</span>
-               <button  onClick={() => this.handelIncrement({id:1})}
-                        className="btn btn-secondary btn-sm">Increment</button>
-           </div>
-   );
-   }
-   getBadgeCLasses()
-   {
-       let classes="badge m-2 badge-";
-       classes+=(this.state.count===0)?"danger" : "primary";
-    return classes;
-   }
+import React, {Component} from 'react';
 
-   formatCount(){
-       const{count}=this.state;
-       return count===0 ?"Zero":count;
-   }
+class Counter extends Component {
+    state = {
+        count: 0,
+        tags: ['tag1', 'tag2', 'tag3']
+    };
+    handelIncrement = product => {
+        console.log(product);
+        this.setState({count: this.state.count + 1})
+    };
+
+    render() {
+        return (
+            <div>
+                <span className={this.getBadgeCLasses()}>{this.formatCount()}</span>
+                <button onClick={() => this.handelIncrement({id: 1})}
+                        className="btn btn-secondary btn-sm">Increment
+                </button>
+            </div>
+        );
+    }
+
+    getBadgeCLasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? "danger" : "primary";
+        return classes;
+    }
+
+    formatCount() {
+        const {count} = this.state;
+        return count === 0 ? "Zero" : count;
+    }
 }
-export  default Counter;
+
+export default Counter;
--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import Counter from './components/counter';
-ReactDom.render(<Counter/>,document.getElementById("root"));
+
+ReactDom.render(<Counter/>, document.getElementById("root"));
